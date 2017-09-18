bde1982c69c05aaf20aef0386936665e550180cf
yong-fu <yong.y.fu@cnsdc01.pwc.com>
2017-09-18 11:26:15 +0800

    Project files updated

---
+++ test_model.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+test_arg="false"
+bleu_arg="-1"
+if [ ! -z $1 ]
+then
+    if [ $1 == "test" ]
+    then
+        test_arg="true"
+    fi
+    if [ $1 == "bleu" ]
+    then
+        if [ ! -z $2 ]
+        then
+            bleu_arg="$2"
+        fi
+    fi
+fi
+
+python s2s.py \
+--size 1024 \
+--num_layers 2 \
+--num_epoch 5 \
+--batch_size 64 \
+--num_per_epoch 500000 \
+--model_dir ./model/model1 \
+--test true
